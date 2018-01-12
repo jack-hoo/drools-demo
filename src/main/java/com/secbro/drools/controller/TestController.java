48c06724ddd61692aec5cfa4a5ee0301d876d764
shugan <shugan@2dfire.com>
2018-01-12 10:04:58 +0800

    second push

--- src/main/java/com/secbro/drools/controller/TestController.java
+++ src/main/java/com/secbro/drools/controller/TestController.java
@@ -51,14 +51,14 @@ public class TestController {
     }
 
     @ResponseBody
-    @RequestMapping("/birthday")
+    @RequestMapping("/point")
     public PointDomain birthday() {
-        System.out.println(System.getProperty("user.dir"));
-        File file = new File("src/main/resources/rules/addpoint.drl");
-        //file.mkdir();
-        System.out.println(file.exists());
         KieSession kieSession = KieUtils.getKieContainer().newKieSession();
+        //KieSession就是一个跟Drools引擎打交道的会话，其基于KieBase创建，它会包含运行时数据，包含“事实Fact”，并对运行时数据实时进行规则运算。通过KieContainer创建KieSession是一种较为方便的做法，其本质上是从KieBase中创建出来的。KieSession就是应用程序跟规则引擎进行交互的会话通道。
+        //创建KieBase是一个成本非常高的事情，KieBase会建立知识（规则、流程）仓库，而创建KieSession则是一个成本非常低的事情，所以KieBase会建立缓存，而KieSession则不必。
+
         PointDomain pointDomain = new PointDomain();
+        //PointDomain为事实Fact
         pointDomain.setBackMondy(200);
         pointDomain.setBackNums(2);
         pointDomain.setBillThisMonth(1);
@@ -66,9 +66,12 @@ public class TestController {
         pointDomain.setBuyMoney(1000);
         pointDomain.setUserName("jack");
         pointDomain.setPoint(100);
+
         kieSession.insert(pointDomain);
         int ruleFiredCount = kieSession.fireAllRules();
+        //载入fact进行推理，Rete算法
         System.out.println("触发了" + ruleFiredCount + "条规则");
+        //释放资源
         kieSession.dispose();
         return pointDomain;
     }
