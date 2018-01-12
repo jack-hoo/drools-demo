792cba57569b6440798d13cb6340ed4068abdf32
shugan <shugan@2dfire.com>
2018-01-12 09:49:40 +0800

    add rules

--- src/main/java/com/secbro/drools/config/DroolsAutoConfiguration.java
+++ src/main/java/com/secbro/drools/config/DroolsAutoConfiguration.java
@@ -16,11 +16,7 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 
 import java.io.IOException;
 
-/**
- * 你的支持是我努力的最大动力！社区的建立离不开你的支持。
- * 此系列课程正在持续更新中，相关讨论QQ（593177274）已经建立，欢迎大家加入讨论。
- * 本人博客地址：http://blog.csdn.net/wo541075754
- */
+
 @Configuration
 public class DroolsAutoConfiguration {
     
--- src/main/java/com/secbro/drools/model/fact/AddressCheckResult.java
+++ src/main/java/com/secbro/drools/model/fact/AddressCheckResult.java
@@ -1,10 +1,6 @@
 package com.secbro.drools.model.fact;
 
-/**
- * 你的支持是我努力的最大动力！社区的建立离不开你的支持。
- * 此系列课程正在持续更新中，相关讨论QQ（593177274）已经建立，欢迎大家加入讨论。
- * 本人博客地址：http://blog.csdn.net/wo541075754
- */
+
 public class AddressCheckResult {
 
     /**
